7b0b2565ae60be8a81e1a3fd04a98c4cba3789c1
Ziyodullo <ziyodillorizayev@gmail.com>
2025-05-06 22:47:35 +0500

    Add project files.

---
+++ Sheenam.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,40 @@
+﻿//================================
+//Copyright (C) Coalitionof Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//================================
+
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Sheenam.Api.Models.Foundations;
+using System.Threading.Tasks;
+
+namespace  Sheenam.Api.Brokers.Storages
+{
+    public partial class StorageBroker: EFxceptionsContext, IStorageBroker
+    {
+        private IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration) 
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+                this.configuration.GetConnectionString(name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }     
+
+    }
+
+}
---
+++ Sheenam.Api/Models/Foundations/GenderType.cs
@@ -0,0 +1,17 @@
+﻿//================================
+//Copyright (C) Coalitionof Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//================================
+
+
+namespace Sheenam.Api.Models.Foundations
+{
+    public enum GenderType
+    {
+        Male,
+        Female,
+        Other,
+        Male1
+
+    }
+}
